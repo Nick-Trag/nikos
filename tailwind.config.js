5b44dce3a7182d160eebbf0860ec2b7c338eea59
Nick-Trag <ntragkas@csd.auth.gr>
2024-07-10 18:28:28 +0300

    Vertically centered the about me section and converted the navbar height to a variable. Also added height breakpoints to Tailwind and used one of them to make the centered text behave well in tiny height screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,26 @@ module.exports = {
       fontFamily: {
         mallanna: ['Mallanna', ...defaultTheme.fontFamily.sans],
         capriola: ['Capriola', 'Mallanna', ...defaultTheme.fontFamily.sans],
+      },
+      screens: {
+        'h-xs': {
+          'raw': '(min-height: 150px)'
+        },
+        'h-sm': {
+          'raw': '(min-height: 300px)'
+        },
+        'h-md': {
+          'raw': '(min-height: 450px)'
+        },
+        'h-lg': {
+          'raw': '(min-height: 600px)'
+        },
+        'h-xl': {
+          'raw': '(min-height: 750px)'
+        },
+        'h-2xl': {
+          'raw': '(min-height: 900px)'
+        },
       }
     },
   },
