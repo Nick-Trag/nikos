4c52bc9b2a9d5f0548db8c642c7c83eb94de58a4
Nick-Trag <ntragkas@csd.auth.gr>
2024-07-31 01:30:32 +0300

    Added a test transition using CSS instead of Angular animations for the experience

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements AfterViewInit {
+  protected inView = false;
 
+  @ViewChild('container') container: ElementRef | undefined;
+
+  ngAfterViewInit(): void {
+    if (this.container !== undefined) {
+      const intersectionObserver = new IntersectionObserver((entries, observer) => {
+        if (entries[0].isIntersecting) {
+          this.inView = true;
+          observer.unobserve(entries[0].target);
+        }
+      }, {threshold: 0.4}); // TODO: Check threshold for small screens
+      intersectionObserver.observe(this.container.nativeElement);
+    }
+    else {
+      this.inView = true;
+    }
+  }
 }
