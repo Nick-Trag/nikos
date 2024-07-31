f89dd9e4a9cefd8a418026657479bbd0d6ee5a47
Nick-Trag <ntragkas@csd.auth.gr>
2024-07-31 15:32:29 +0300

    Using signals in education and experience, which should help me encapsulate the code

--- src/app/education/education.component.ts
+++ src/app/education/education.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, signal, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-education',
@@ -27,7 +27,7 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
   ]
 })
 export class EducationComponent implements AfterViewInit {
-  protected inView = false;
+  protected inView = signal(false);
 
   @ViewChild('container') container: ElementRef | undefined;
 
@@ -36,14 +36,14 @@ export class EducationComponent implements AfterViewInit {
     if (this.container !== undefined) {
       const intersectionObserver = new IntersectionObserver((entries, observer) => {
         if (entries[0].isIntersecting) {
-          this.inView = true;
+          this.inView.set(true);
           observer.unobserve(entries[0].target);
         }
       }, {threshold: 0.4}); // TODO: Check threshold for small screens
       intersectionObserver.observe(this.container.nativeElement);
     }
     else {
-      this.inView = true;
+      this.inView.set(true);
     }
   }
 }
--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, signal, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -8,7 +8,7 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
   styleUrl: './experience.component.scss'
 })
 export class ExperienceComponent implements AfterViewInit {
-  protected inView = false;
+  protected inView = signal(false);
 
   @ViewChild('container') container: ElementRef | undefined;
 
@@ -16,14 +16,14 @@ export class ExperienceComponent implements AfterViewInit {
     if (this.container !== undefined) {
       const intersectionObserver = new IntersectionObserver((entries, observer) => {
         if (entries[0].isIntersecting) {
-          this.inView = true;
+          this.inView.set(true);
           observer.unobserve(entries[0].target);
         }
       }, {threshold: 0.4}); // TODO: Check threshold for small screens
       intersectionObserver.observe(this.container.nativeElement);
     }
     else {
-      this.inView = true;
+      this.inView.set(true);
     }
   }
 }
