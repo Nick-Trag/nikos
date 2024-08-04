ab2745af4790a6b9c186b4e76619d9813f61789b
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-04 13:01:07 +0300

    Added a clip path to the mobile navbar, but still got a lot to fix on it

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
