b578055714fb5bead928995726aea94e03866cae
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-12 21:16:47 +0300

    Created the Photography component, and made the navbar close when a link is pressed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
+import { PhotographyComponent } from "./photography/photography.component";
 
 export const routes: Routes = [
+  {
+    path: 'photography',
+    component: PhotographyComponent,
+  },
   {
     path: '',
     component: HomeComponent,
