2aece4e0acf7741562885a647157683e243ec24d
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-13 23:55:36 +0300

    Added alt text language

--- src/app/photos.ts
+++ src/app/photos.ts
@@ -1,63 +1,78 @@
 export interface Photo {
   name: string;
   alt: string;
+  lang: string;
 }
 
 export const photos: Photo[] = [
   {
     name: 'DSC_0788.jpg',
     alt: 'Belém Tower',
+    lang: 'en',
   },
   {
     name: 'DSC_0191_crop.jpg',
     alt: 'Retiro Park',
+    lang: 'en',
   },
   {
     name: 'DSC_0065.jpg',
     alt: 'Statue of Alexander the Great',
+    lang: 'en',
   },
   {
     name: 'DSC_0186.jpg',
     alt: 'Palace of Culture and Science',
+    lang: 'en',
   },
   {
     name: 'DSC_0269.jpg',
     alt: 'Palacio de Cristal',
+    lang: 'es',
   },
   {
     name: 'DSC_0351.jpg',
     alt: 'Sevilla buildings on the river',
+    lang: 'en',
   },
   {
     name: 'DSC_0497.jpg',
     alt: 'Zurich',
+    lang: 'en',
   },
   {
     name: 'DSC_0555.jpg',
     alt: 'Plaza de España',
+    lang: 'es',
   },
   {
     name: 'DSC_0587.jpg',
     alt: 'Tel Aviv skyscrapers',
+    lang: 'en',
   },
   {
     name: 'DSC_0693.jpg',
     alt: 'Plaza de España',
+    lang: 'es',
   },
   {
     name: 'DSC_0773.jpg',
     alt: 'Monolithos Castle',
+    lang: 'en',
   },
   {
     name: 'DSC_0953_1.jpg',
     alt: 'Bird flying in front of bridge',
+    lang: 'en',
   },
   {
     name: 'DSC_1158.jpg',
     alt: 'Walls of Dubrovnik',
+    lang: 'en',
   },
   {
     name: 'DSC_0276.jpg',
     alt: 'Cloudy sunset at Peles Castle',
+    lang: 'en',
   },
 ];
