5a382d874d31314ffe3ece8372443de361dc1746
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-21 12:53:10 +0300

    Shuffling the photos array, to get them in random order

--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -12,5 +12,24 @@ import { NgClass } from "@angular/common";
   styleUrl: './photography.component.scss'
 })
 export class PhotographyComponent {
-  photos: Photo[] = photos;
+  photos: Photo[] = this.shuffle(photos);
+
+  // Shuffle an array and return the shuffled version. Reference: https://stackoverflow.com/a/2450976/7400287
+  private shuffle(array: Photo[]) {
+    const photos = array.slice();
+    let currentIndex = photos.length;
+
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [photos[currentIndex], photos[randomIndex]] = [
+        photos[randomIndex], photos[currentIndex]];
+    }
+    return photos;
+  }
 }
