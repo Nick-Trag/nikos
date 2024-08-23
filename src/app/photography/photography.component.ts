a968e3077e621c57cc272a87e532ce1d762167af
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-23 21:37:46 +0300

    Moved initialization logic to ngOnInit and added translation and rotation arrays, which will be semi-randomized

--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Photo, photos } from "../photos";
 import { NgClass } from "@angular/common";
 
@@ -11,8 +11,18 @@ import { NgClass } from "@angular/common";
   templateUrl: './photography.component.html',
   styleUrl: './photography.component.scss'
 })
-export class PhotographyComponent {
-  photos: Photo[] = this.shuffle(photos);
+export class PhotographyComponent implements OnInit {
+  photos: Photo[] = [];
+  rotations: number[] = [];
+  translations: number[] = [];
+
+  ngOnInit(): void {
+    this.photos = this.shuffle(photos);
+    // TODO: Semi-random translations and rotations
+    this.rotations = Array(this.photos.length).fill(20);
+    this.translations = Array(this.photos.length).fill(200);
+  }
+
 
   // Shuffle an array and return the shuffled version. Reference: https://stackoverflow.com/a/2450976/7400287
   private shuffle(array: Photo[]) {
