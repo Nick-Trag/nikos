e42584d16c9fe0e6085fef43d66d079e122119e4
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-24 03:58:13 +0300

    Restricted maximum width of the photo container and added proper translations, rotations and z-indices

--- src/app/photography/photography.component.ts
+++ src/app/photography/photography.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Photo, photos } from "../photos";
-import { NgClass } from "@angular/common";
+import { NgClass, NgStyle } from "@angular/common";
 
 @Component({
   selector: 'app-photography',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    NgStyle
   ],
   templateUrl: './photography.component.html',
   styleUrl: './photography.component.scss'
@@ -14,16 +15,18 @@ import { NgClass } from "@angular/common";
 export class PhotographyComponent implements OnInit {
   photos: Photo[] = [];
   rotations: number[] = [];
-  translations: number[] = [];
+  translationsX: number[] = [];
+  translationsY: number[] = [];
+  zIndices: number[] = [];
+  // styles: object[] = [];
 
   ngOnInit(): void {
     this.photos = this.shuffle(photos);
-    // TODO: Semi-random translations and rotations
-    // Maybe just the rotations, and use flex-wrap instead. Because mobile is low-key a nightmare
-    // Or maybe actually straight up display: none on the container when on mobile and make a completely different UI. This might be better actually
     // TODO: Animate
-    for (let i = 0; i < this.photos.length; i++) { // Need a minimum rotation at least
-      this.rotations.push(Math.random() * 70 - 35);
+    for (let i = 0; i < this.photos.length; i++) {
+      this.rotations.push(Math.random() * 70 - 35); // Rotations from -35deg to 35deg
+
+      // Polaroids look bad if they are too straight, so enforce a 15-degree rotation minimum
       if (this.rotations[i] > 0 && this.rotations[i] < 15) {
         this.rotations[i] = 15;
       }
@@ -31,7 +34,15 @@ export class PhotographyComponent implements OnInit {
         this.rotations[i] = -15;
       }
 
-      this.translations.push(Math.random() * 100 - 50);
+      this.translationsX.push(Math.random() * 100 - 50); // Translations from -50px to 50px
+      this.translationsY.push(Math.random() * 100 - 50);
+      this.zIndices.push(Math.floor(Math.random() * 20) + 1); // TODO: Not sure if I want random z-indices
+
+      // this.styles.push({
+      //   'z-index': this.zIndices[i],
+      //   'translate': this.translationsX[i] + 'px ' + this.translationsY[i] + 'px',
+      //   'rotate': this.rotations[i] + 'deg',
+      // });
     }
   }
 
@@ -49,8 +60,7 @@ export class PhotographyComponent implements OnInit {
       currentIndex--;
 
       // And swap it with the current element.
-      [photos[currentIndex], photos[randomIndex]] = [
-        photos[randomIndex], photos[currentIndex]];
+      [photos[currentIndex], photos[randomIndex]] = [photos[randomIndex], photos[currentIndex]];
     }
     return photos;
   }
