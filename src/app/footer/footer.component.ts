e2abdde89007ad16e7dc35e254df400caa12bc00
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-25 23:26:04 +0300

    Added a fade animation to the footer items and refactored the slide animations

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { ObserverDirective } from "../observer.directive";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    ObserverDirective
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-
+  protected inView = signal(false);
 }
