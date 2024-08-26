193fed20d6e48f1edd2c5ad9b9780e59ff36ae8b
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-26 19:03:00 +0300

    Cleaned up mobile navbar code by using Angular animations

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { NgClass } from "@angular/common";
+import { animate, state, style, transition, trigger } from "@angular/animations";
+
+const open = { 'clip-path': 'ellipse(1100px 1100px at 0% 0%)' };
+const closed = { 'clip-path': 'ellipse(0px 0px at -100px -100px)' };
+const timing: string = '0.3s ease-in-out';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +15,19 @@ import { NgClass } from "@angular/common";
     NgClass
   ],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
+  animations: [
+    trigger('toggleNav', [
+      transition(':enter', [
+        style(closed),
+        animate(timing, style(open)),
+      ]),
+      transition(':leave', [
+        style(open),
+        animate(timing, style(closed)),
+      ]),
+    ]),
+  ],
 })
 export class NavbarComponent {
   protected navOpen = false;
