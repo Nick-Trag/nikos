b9e43f9c80b3b1bd34df47b3854ae1cb4530ae07
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-29 20:10:18 +0300

    Added an initial loader, that looks ugly, but seems to be working decently (still tied to only a setTimeout, not an actual load)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,28 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { RoutedService } from "./routed.service";
+import { LoadingComponent } from "./loading/loading.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, LoadingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'nikos';
+export class AppComponent implements OnInit {
+  loading = true;
   private routedService = inject(RoutedService); // We need to start the service from here, so that it starts tracking from the start
 
   constructor() {
     this.routedService.isFirstLoadedPage(); // Doesn't actually do anything, just forces Angular to use the service
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;
+    }, 1000);
   }
 }
