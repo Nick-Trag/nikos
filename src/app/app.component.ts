1f643506e0dd2efc8441f482500052de0698809c
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-30 00:33:24 +0300

    Removed the loading component and added the loader in the AppComponent, so now the animations are working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,15 +3,34 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { FooterComponent } from "./footer/footer.component";
 import { RoutedService } from "./routed.service";
-import { LoadingComponent } from "./loading/loading.component";
 import { ImageLoaderService } from "./image-loader.service";
+import { NgOptimizedImage } from "@angular/common";
+import { animate, group, query, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, LoadingComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
+  animations: [ // TODO: Make the animations a bit smoother
+    trigger('enterLeave', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('1s ease-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        group([
+          query('.top-half', [
+            animate('0.5s ease-in', style({ translate: '0 -700px' })),
+          ]),
+          query('.bottom-half', [
+            animate('0.5s ease-in', style({ translate: '0 700px' })),
+          ]),
+        ]),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent implements OnInit {
   loading = true;
@@ -23,6 +42,7 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // TODO: Preferably not hardcoded to 5 seconds (either less, or dynamic, probably just less)
     setTimeout(() => {
       this.loading = false;
     }, 5000);
