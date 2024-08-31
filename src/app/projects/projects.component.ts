2efad29c783f63dff1a11c76a7d5e9e1d1cf0af3
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-31 20:04:07 +0300

    Added a recursion Easter egg in the projects component

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -2,6 +2,7 @@ import { Component, signal, WritableSignal } from '@angular/core';
 import { Project, projects } from "../projects";
 import { ObserverDirective } from "../observer.directive";
 import { NgOptimizedImage } from "@angular/common";
+import { animate, style, transition, trigger } from "@angular/animations";
 
 @Component({
   selector: 'app-projects',
@@ -12,11 +13,24 @@ import { NgOptimizedImage } from "@angular/common";
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss',
+  animations: [
+    trigger('recursion', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('0.5s ease-in-out', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1 }),
+        animate('0.5s ease-in-out', style({ opacity: 0 })),
+      ]),
+    ]),
+  ],
 })
 export class ProjectsComponent {
   projects: Project[] = projects;
   inView: WritableSignal<boolean>[] = Array.from(this.projects, () => signal(false));
   protected threshold = window.innerWidth > 768 ? 0.4 : 0.2; // A smaller threshold for smaller screens (smaller than md)
+  showRecursion = false;
 
   linkClicked(event: MouseEvent, link: string) {
     if (link === "#") {
@@ -27,7 +41,10 @@ export class ProjectsComponent {
   }
 
   recurse(): void {
-    // TODO: Show some modal or an icon or something about recursion
     window.scrollTo({top: 0, behavior: "smooth"});
+    this.showRecursion = true;
+    setTimeout(() => {
+      this.showRecursion = false;
+    }, 2000);
   }
 }
