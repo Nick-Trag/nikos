76633bb5f00d49879f456b15bdd1ba923c6d034d
Nick-Trag <ntragkas@csd.auth.gr>
2024-08-31 21:00:17 +0300

    Added fade-in animation to 404 page and under construction page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NgClass } from "@angular/common";
+import { LoadingScreenService } from "../loading-screen.service";
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.scss'
+  styleUrl: './page-not-found.component.scss',
 })
 export class PageNotFoundComponent {
-
+  private loadingScreenService = inject(LoadingScreenService);
+  protected loadingScreenShown = this.loadingScreenService.hasLoadingScreenBeenShown();
 }
--- src/app/under-construction/under-construction.component.ts
+++ src/app/under-construction/under-construction.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { LoadingScreenService } from "../loading-screen.service";
+import { NgClass } from "@angular/common";
 
 @Component({
   selector: 'app-under-construction',
   standalone: true,
-  imports: [],
+  imports: [
+    NgClass
+  ],
   templateUrl: './under-construction.component.html',
   styleUrl: './under-construction.component.scss'
 })
 export class UnderConstructionComponent {
-
+  private loadingScreenService = inject(LoadingScreenService);
+  protected loadingScreenShown = this.loadingScreenService.hasLoadingScreenBeenShown();
 }
