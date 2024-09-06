6aef732047a8dde94f4c5d455f5d8ce6012d23e0
Nick-Trag <ntragkas@csd.auth.gr>
2024-09-06 13:47:53 +0200

    Created a travel component and routed to it

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HomeComponent } from "./home/home.component";
 import { PhotographyComponent } from "./photography/photography.component";
 import { UnderConstructionComponent } from "./under-construction/under-construction.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { TravelComponent } from "./travel/travel.component";
 
 export const routes: Routes = [ // TODO: Route animations
   {
@@ -19,7 +20,7 @@ export const routes: Routes = [ // TODO: Route animations
   },
   {
     path: 'travel',
-    component: UnderConstructionComponent,
+    component: TravelComponent,
   },
   {
     path: 'gym',
