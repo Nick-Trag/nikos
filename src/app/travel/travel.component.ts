936b6bc0136035f668f1c9febfc0976a94bc2092
Nick-Trag <ntragkas@csd.auth.gr>
2024-09-09 16:20:46 +0200

    Added Leaflet and started creating the map div

--- src/app/travel/travel.component.ts
+++ src/app/travel/travel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-travel',
@@ -8,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrl: './travel.component.scss'
 })
 export class TravelComponent {
-
+  @ViewChild('map')
+  private mapElement!: ElementRef<HTMLElement>;
 }
