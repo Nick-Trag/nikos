621ed4c5f91c157c09653fdafff8a313401c664b
Nick-Trag <ntragkas@csd.auth.gr>
2024-09-23 20:10:22 +0300

    Starting to make the terminal slightly functional

--- src/app/coding/coding.component.ts
+++ src/app/coding/coding.component.ts
@@ -1,12 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
+interface Command {
+  command: string;
+  directory: string;
+  result: string;
+}
 
 @Component({
   selector: 'app-coding',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './coding.component.html',
-  styleUrl: './coding.component.scss'
+  styleUrl: './coding.component.scss',
 })
-export class CodingComponent {
+export class CodingComponent implements OnInit {
+  currentDirectory: string = "~";
+  commandHistory: Command[] = [ // Sample history to make sure it is working. Will start blank, or with some help command(s)
+    {
+      command: 'pwd',
+      directory: '~',
+      result: '~',
+    },
+    {
+      command: 'ls',
+      directory: '~',
+      result: `fileA.txt\nfileB.txt\nfileC.txt`,
+    }
+  ];
+  currentCommand: string = '';
+
+  ngOnInit(): void {
+
+  }
+
+  handleCommand(): void {
+    const command: string = this.currentCommand;
+
+    switch (command) {
+      case "pwd":
+        console.log(this.currentDirectory);
+        break;
+      case "clear":
+        this.commandHistory = [];
+        break;
+      default:
+        console.log(command + " has not been implemented");
+        break;
+    }
+
+    this.currentCommand = '';
+  }
 
 }
