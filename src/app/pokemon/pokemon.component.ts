8483cd52d165fab85b983e6a2f227267885214af
Nick-Trag <ntragkas@csd.auth.gr>
2024-10-20 19:56:15 +0300

    Added Pokémon trainer cards

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.scss'
 })
