5bf83439970905cd5d3b222161e4a55ba80d81de
Nick-Trag <ntragkas@csd.auth.gr>
2024-10-23 18:21:02 +0300

    Added Hollow Knight and Other Games sections

---
+++ src/app/hollow-knight/hollow-knight.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hollow-knight',
+  standalone: true,
+  imports: [],
+  templateUrl: './hollow-knight.component.html',
+  styleUrl: './hollow-knight.component.scss'
+})
+export class HollowKnightComponent {
+
+}
