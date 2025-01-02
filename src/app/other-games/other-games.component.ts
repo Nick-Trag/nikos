9d9519f408f60dc7903c5471084bbbf0853c7588
Nick-Trag <ntragkas@csd.auth.gr>
2025-01-02 20:44:20 +0200

    Added mock Steam games

--- src/app/other-games/other-games.component.ts
+++ src/app/other-games/other-games.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
+import { SteamGame } from "../steam-game";
 
 @Component({
   selector: 'app-other-games',
@@ -11,5 +12,32 @@ import { NgOptimizedImage } from "@angular/common";
   styleUrl: './other-games.component.scss'
 })
 export class OtherGamesComponent {
+  mockGames: SteamGame[] = [
+    {
+      name: 'The Witcher 3: Wild Hunt',
+      hoursPlayed: 178.6,
+      appId: 292030,
+    },
+    {
+      name: 'Elden Ring',
+      hoursPlayed: 134.4,
+      appId: 1245620,
+    },
+    {
+      name: 'Hollow Knight',
+      hoursPlayed: 126.4,
+      appId: 367520,
+    },
+    {
+      name: 'Hogwarts Legacy',
+      hoursPlayed: 100.5,
+      appId: 990080,
+    },
+    {
+      name: 'Ruined King: A League of Legends Story™',
+      hoursPlayed: 40,
+      appId: 1276790,
+    },
+  ];
 
 }
---
+++ src/app/steam-game.ts
@@ -0,0 +1,5 @@
+export interface SteamGame {
+  name: string;
+  hoursPlayed: number;
+  appId: number;
+}
